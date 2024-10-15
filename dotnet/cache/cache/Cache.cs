00f819885ac2a8a0e2ab3ef2ef1b15a9201c22a0
Denis Kralj <denis-kralj@users.noreply.github.com>
2024-10-15 17:03:43 +0200

    refactor: move storage of last accessed data from linked list to dictionary
    Add type CacheEntry
    Remove type NodeValue
    Remove use of linq First()
    Move last accessed update logic out of move to head helper
    Refactor Has() to use TryGetValue
    Update journal

--- dotnet/cache/cache/Cache.cs
+++ dotnet/cache/cache/Cache.cs
@@ -3,8 +3,8 @@
 public class Cache<T>(int itemCapacity = 3, int timeToLiveMilliseconds = 5000, Func<DateTime>? nowProvider = null) : ICache<T> where T : class
 {
     private static Func<DateTime> _defaultNowProvider = () => DateTime.UtcNow;
-    private readonly Dictionary<string, T> _cache = new(itemCapacity);
-    private readonly LinkedList<NodeValue> _entryKeyList = new();
+    private readonly Dictionary<string, CacheEntry> _cache = new(itemCapacity);
+    private readonly LinkedList<string> _entryKeyList = new();
     private readonly int _itemCapacity = itemCapacity;
     private readonly int _timeToLiveMilliseconds = timeToLiveMilliseconds;
     private readonly Func<DateTime> _nowProvider = nowProvider ?? _defaultNowProvider;
@@ -14,7 +14,8 @@ public class Cache<T>(int itemCapacity = 3, int timeToLiveMilliseconds = 5000, F
     {
         if(_cache.TryGetValue(key, out var value)) {
             MoveToHead(key);
-            return value;
+            value.LastAccessed = _nowProvider();
+            return value.Entry;
         }
 
         return null;
@@ -23,18 +24,23 @@ public class Cache<T>(int itemCapacity = 3, int timeToLiveMilliseconds = 5000, F
     public bool Has(string key)
     {
         ExpireIfNeeded();
-        var has = _cache.ContainsKey(key);
-        if(has)
-        {
+        if(_cache.TryGetValue(key, out var value)) {
             MoveToHead(key);
+            value.LastAccessed = _nowProvider();
+            return true;
         }
-        return has;
+
+        return false;
     }
 
     private void ExpireIfNeeded()
     {
-        while (_entryKeyList.Last != null && IsExpired(_entryKeyList.Last.Value.LastAccessed)) {
-            _cache.Remove(_entryKeyList.Last.Value.Key);
+        while (
+            _entryKeyList.Last != null &&
+            _cache.TryGetValue(_entryKeyList.Last.Value, out var entry) &&
+            IsExpired(entry.LastAccessed))
+        {
+            _cache.Remove(_entryKeyList.Last.Value);
             _entryKeyList.RemoveLast();
         }
     }
@@ -46,28 +52,27 @@ public class Cache<T>(int itemCapacity = 3, int timeToLiveMilliseconds = 5000, F
 
     private void MoveToHead(string key)
     {
-        if (key == _entryKeyList.First?.Value.Key)
+        if (key == _entryKeyList.First?.Value)
         {
             return;
         }
-        var entry =_entryKeyList.First(e => e.Key == key);
-        _entryKeyList.Remove(entry);
-        entry.LastAccessed = _nowProvider();
-        _entryKeyList.AddFirst(entry);
+        
+        _entryKeyList.Remove(key);
+        _entryKeyList.AddFirst(key);
     }
 
     public void Set(string key, T value)
     {
         RemoveIfExists(key);
         EvictIfNeeded();
-        _cache.Add(key, value);
-        _entryKeyList.AddFirst(new NodeValue { Key = key, LastAccessed = _nowProvider() });
+        _cache.Add(key, new CacheEntry { Entry = value, LastAccessed = _nowProvider() });
+        _entryKeyList.AddFirst(key);
     }
 
     private void EvictIfNeeded()
     {
         while (_itemCapacity <= _entryKeyList.Count && _entryKeyList.Last != null) {
-            _cache.Remove(_entryKeyList.Last.Value.Key);
+            _cache.Remove(_entryKeyList.Last.Value);
             _entryKeyList.RemoveLast();
         }
     }
@@ -76,14 +81,13 @@ public class Cache<T>(int itemCapacity = 3, int timeToLiveMilliseconds = 5000, F
     {
         if(_cache.ContainsKey(key)) {
             _cache.Remove(key);
-            var entry = _entryKeyList.First(e => e.Key == key);
-            _entryKeyList.Remove(entry);
+            _entryKeyList.Remove(key);
         }
     }
 
-    class NodeValue
+    class CacheEntry
     {
-        public required string Key { get; set; }
+        public required T Entry { get; set; }
         public DateTime LastAccessed { get; set; }
     }
 }
--- dotnet/journal.md
+++ dotnet/journal.md
@@ -22,11 +22,11 @@
 - change internal linked list to store last accessed information
 - introduced now provider
 - refactor code to work with new nodes in linked list
-
-
+- refactor storage of last access information
 # decisions
 
 - choose nunit over xunit because the documentation was easier to read and the assertion writing patterns were more semantic
 - decided to limit T in the cache to classes, this means that structs aren't supported in the cache, this simplifies my get logic and removes chances to return default primitives for missing cache keys
 - storing actual cached valus in dictionary (quick access) and using a linked list to determine eviction/expiration
 - added support for supplying a now provider, this is used for testing so that we don't have to await time in the test, but rather fake the passage of time
+- refactored implementation of last access storage because this enabled removal of linq First() method, in theory saving time
