d0e0cea20b0823391b245dde1147c79722f7f3d5
Denis Kralj <denis-kralj@users.noreply.github.com>
2024-10-16 15:51:45 +0200

    feat: naive implementation of cache
    Add get method implementation
    Add set method implementation
    Add has method implementation
    Add tests

--- node/cache/cache.test.ts
+++ node/cache/cache.test.ts
@@ -1,7 +1,25 @@
-import { it, expect } from 'vitest';
-import { createCache } from './cache';
+import { it, expect, describe } from 'vitest'
+import { createCache } from './cache'
 
-it('dummy test', () => {
-    const cache = createCache();
-    expect(cache).toBeDefined();
+describe('`has()` method', () => {
+    it('returns `false` for non existing key', () => {
+        const cache = createCache()
+        expect(cache.has('nonExistingKey')).toBe(false)
+    })
+})
+
+describe('`set()` method', () => {
+    it('saves key to cache', () => {
+        const cache = createCache()
+        cache.set('key1', 'value1')
+        expect(cache.has('key1')).toBe(true)
+    })
+})
+
+describe('`get()` method', () => {
+    it('retrieves correct value from cache', () => {
+        const cache = createCache()
+        cache.set('key1', 'value1')
+        expect(cache.get('key1')).toBe('value1')
+    })
 })
--- node/cache/cache.ts
+++ node/cache/cache.ts
@@ -1,7 +1,8 @@
-export function createCache() {
+export function createCache<T>() {
+    const _store = new Map<string, T>()
     return {
-        get: () => {},
-        set: () => {},
-        has: () => {},
+        get: (key: string): (T | null) => { return _store.get(key) ?? null },
+        set: (key: string, value: T) => { _store.set(key, value) },
+        has: (key: string) => { return _store.has(key) },
     }
 }
