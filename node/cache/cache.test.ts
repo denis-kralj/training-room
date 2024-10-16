15ba6f4bf25785a8b4c17d7a408ede82b2d1858e
Denis Kralj <denis-kralj@users.noreply.github.com>
2024-10-16 16:22:06 +0200

    feat: support item limit in cache
    Add item limit to cache building function
    Set item limit default to 10
    Add LinkedListCacheEntry type
    Add head and tail to cache to track order of eviction based on capacity
    Add set logic for entry eviction
    Store entry information in linked list fields
    Add tests

--- node/cache/cache.test.ts
+++ node/cache/cache.test.ts
@@ -14,6 +14,25 @@ describe('`set()` method', () => {
         cache.set('key1', 'value1')
         expect(cache.has('key1')).toBe(true)
     })
+    it('evicts entries when capacity is reached', () => {
+        const cache = createCache(1)
+        cache.set('key1', 'value1')
+        cache.set('key2', 'value2')
+        expect(cache.has('key1')).toBe(false)
+        expect(cache.has('key2')).toBe(true)
+    })
+    it('overwrites value for existing key in cache', () => {
+        const cache = createCache()
+        cache.set('key1', 'value1')
+        cache.set('key1', 'value11')
+        expect(cache.get('key1')).toBe('value11')
+    })
+    it('overwrites value for existing key in cache considering eviction', () => {
+        const cache = createCache(1)
+        cache.set('key1', 'value1')
+        cache.set('key1', 'value11')
+        expect(cache.get('key1')).toBe('value11')
+    })
 })
 
 describe('`get()` method', () => {
--- node/cache/cache.ts
+++ node/cache/cache.ts
@@ -1,8 +1,40 @@
-export function createCache<T>() {
+type LinkedListCacheEntry<T> = {
+    value: T
+    key: string
+    head: LinkedListCacheEntry<T> | null
+    tail: LinkedListCacheEntry<T> | null
+}
+
+export function createCache<T>(itemLimit: number = 10) {
+    const _itemLimit = itemLimit
     const _store = new Map<string, T>()
+    let _head: (LinkedListCacheEntry<T> | null) = null
+    let _tail: (LinkedListCacheEntry<T> | null) = null
+
+    const evictIfNeeded = () => {
+        while (_tail !== null && _store.size >= _itemLimit) {
+            _store.delete(_tail.key)
+            _tail = _tail.head
+            if(_store.size === 0) {
+                _head = null
+            }
+        }
+    }
+
     return {
         get: (key: string): (T | null) => { return _store.get(key) ?? null },
-        set: (key: string, value: T) => { _store.set(key, value) },
         has: (key: string) => { return _store.has(key) },
+        set: (key: string, value: T) => {
+            evictIfNeeded();
+            _store.set(key, value)
+            const entry: LinkedListCacheEntry<T> = { value: value, key: key, head: null, tail: _head }
+            if (_head !== null) {
+                _head.head = entry
+            }
+            if (_tail === null) {
+                _tail = entry
+            }
+            _head = entry
+        },
     }
 }
